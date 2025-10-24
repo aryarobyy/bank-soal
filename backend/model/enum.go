426e4e409c3ca70b4b75d5b704f82c8ca5cf85bd
aryarobyy <robyaryanata12345@gmail.com>
2025-10-24 13:21:07 +0700

    (+) add subject

--- backend/model/enum.go
+++ backend/model/enum.go
@@ -3,6 +3,7 @@ package model
 type Difficulty string
 type Role string
 type Status string
+type SubjectTitle string
 
 const (
 	DifficultyEasy   Difficulty = "easy"
@@ -20,4 +21,12 @@ const (
 	RoleUser       Role = "user"
 	RoleSuperAdmin Role = "super_admin"
 	RoleLecturer   Role = "lecturer"
+)
+
+const (
+	SubjectKalkulus SubjectTitle = "Kalkulus"
+	SubjectMatDis   SubjectTitle = "Matematika Diskrit"
+	SubjectAutomata SubjectTitle = "Teori Bahasa dan Automata"
+	SubjectData     SubjectTitle = "Basis Data Lanjut"
+	SubjectMetNum   SubjectTitle = "Metode Numerik"
 )
