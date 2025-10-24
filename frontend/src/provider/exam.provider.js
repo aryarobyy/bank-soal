94fc3c68d82afac68021586f3b9227ac0ff7c0c1
Muhammad Ryan Al Habsy <mryanalhabsy@gmail.com>
2025-10-24 21:35:30 +0700

    IntegratedExam

--- frontend/src/provider/exam.provider.js
+++ frontend/src/provider/exam.provider.js
@@ -2,26 +2,26 @@ import { EXAM } from "../core/constant";
 import ApiHandler from "./api.handler";
 
 export const getAllExam = async () => {
-  const res = await ApiHandler.get(`${EXAM}`);
+  const res = await ApiHandler.get(`/${EXAM}`);
   return res.data;
 };
 
 export const getExamById = async (id) => {
-  const res = await ApiHandler.get(`${EXAM}/id?id=${id}`);
+  const res = await ApiHandler.get(`/${EXAM}/id?id=${id}`);
   return res.data;
 };
 
 export const createExam = async (data) => {
-  const res = await ApiHandler.post(`${EXAM}`, data);
+  const res = await ApiHandler.post(`/${EXAM}`, data);
   return res.data;
 };
 
 export const updateExam = async (id, data) => {
-  const res = await ApiHandler.put(`${EXAM}/id?id=${id}`, data);
+  const res = await ApiHandler.put(`/${EXAM}/id?id=${id}`, data);
   return res.data;
 };
 
 export const deleteExam = async (id) => {
-  const res = await ApiHandler.delete(`${EXAM}/id?id=${id}`);
+  const res = await ApiHandler.delete(`/${EXAM}/id?id=${id}`);
   return res.data;
 };
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,74 +1,89 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
+import DosenLayout from "../layouts/DosenLayout.vue";
 
-import DosenLayout from '../layouts/DosenLayout.vue'
+import LoginView from "../views/Auth/LoginView.vue";
+import RegisterView from "../views/Auth/RegisterView.vue";
+import ForgotPassword from "../views/Auth/ForgotPassword.vue";
 
+import HomePage from "../views/home/HomeView.vue";
+import ExamView from "../views/exam/ExamView.vue";
+import StudentProfilePage from "../views/Super Admin/SuperAdminProfile.vue";
+import UpdateProfile from "../views/profile/UpdateProfile.vue";
 
-import LoginView from '../views/Auth/LoginView.vue'
-import RegisterView from '../views/Auth/RegisterView.vue'
-import ForgotPassword from '../views/Auth/ForgotPassword.vue'
+import Profile from "../views/profile/Profile.vue";
 
-
-import HomePage from '../views/home/HomeView.vue'
-import ExamView from '../views/exam/ExamView.vue'
-import StudentProfilePage from '../views/Super Admin/SuperAdminProfile.vue'
-import UpdateProfile from '../views/profile/UpdateProfile.vue'
-
-
-import Profile from '../views/profile/Profile.vue' 
-
-
-import LecturerDashboard from '../views/dosen/LecturerDashboard.vue'
-import LecturerProfilePage from '../views/dosen/LecturerProfilePage.vue'
+import LecturerDashboard from "../views/dosen/LecturerDashboard.vue";
+import LecturerProfilePage from "../views/dosen/LecturerProfilePage.vue";
 
 // Halaman Soal
-import LecturerSoal from '../views/question/LecturerSoal.vue'
-import LecturerSoalList from '../views/question/LecturerSoalList.vue'
-import CreateSoal from '../views/question/CreateSoal.vue'
-import CreateManualSoal from '../views/question/CreateManualSoal.vue'
-import UploadJsonSoal from '../views/question/UploadJsonSoal.vue'
+import LecturerSoal from "../views/question/LecturerSoal.vue";
+import LecturerSoalList from "../views/question/LecturerSoalList.vue";
+import CreateSoal from "../views/question/CreateSoal.vue";
+import CreateManualSoal from "../views/question/CreateManualSoal.vue";
+import UploadJsonSoal from "../views/question/UploadJsonSoal.vue";
 
 const routes = [
-
-  { path: '/', name: 'HomePage', component: HomePage },
-  { path: '/login', name: 'login', component: LoginView },
-  { path: '/register', name: 'register', component: RegisterView },
-  { path: '/forgot-password', name: 'ForgotPassword', component: ForgotPassword },
-  { path: '/ujian', name: 'ExamPage', component: ExamView },
-
-  
+  { path: "/", name: "HomePage", component: HomePage },
+  { path: "/login", name: "login", component: LoginView },
+  { path: "/register", name: "register", component: RegisterView },
   {
-    path: '/profile/:id', 
-    name: 'Profile',
-    component: Profile
+    path: "/forgot-password",
+    name: "ForgotPassword",
+    component: ForgotPassword,
   },
-  { 
-    path: '/update-profile', 
-    name: 'UpdateProfile', 
-    component: UpdateProfile 
-  },
+  { path: "/ujian", name: "ExamPage", component: ExamView },
 
-
-  
   {
-    path: '/dosen',
+    path: "/profile/:id",
+    name: "Profile",
+    component: Profile,
+  },
+  {
+    path: "/update-profile",
+    name: "UpdateProfile",
+    component: UpdateProfile,
+  },
+
+  {
+    path: "/dosen",
     component: DosenLayout,
-    redirect: '/dosen/dashboard',
+    redirect: "/dosen/dashboard",
     children: [
-      { path: 'dashboard', name: 'LecturerDashboard', component: LecturerDashboard },
-      { path: 'soal', name: 'LecturerSoal', component: LecturerSoal },
-      { path: 'soal/list', name: 'LecturerSoalList', component: LecturerSoalList },
-      { path: 'soal/create', name: 'CreateSoal', component: CreateSoal },
-      { path: 'soal/create-manual', name: 'CreateManualSoal', component: CreateManualSoal },
-      { path: 'soal/upload-json', name: 'UploadJsonSoal', component: UploadJsonSoal },
-      { path: 'update-profile', name: 'LecturerProfilePage', component: LecturerProfilePage },
-    ]
-  }
-]
+      {
+        path: "dashboard",
+        name: "LecturerDashboard",
+        component: LecturerDashboard,
+      },
+      { path: "soal", name: "LecturerSoal", component: LecturerSoal },
+      {
+        path: "soal/list",
+        name: "LecturerSoalList",
+        component: LecturerSoalList,
+      },
+      { path: "soal/create", name: "CreateSoal", component: CreateSoal },
+      {
+        path: "soal/create-manual",
+        name: "CreateManualSoal",
+        component: CreateManualSoal,
+      },
+      {
+        path: "soal/upload-json",
+        name: "UploadJsonSoal",
+        component: UploadJsonSoal,
+      },
+      {
+        path: "update-profile",
+        name: "LecturerProfilePage",
+        component: LecturerProfilePage,
+      },
+    ],
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
