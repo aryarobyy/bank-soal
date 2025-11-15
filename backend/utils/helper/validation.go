420855919a384ee9fb0d831fd391eedb14a68b39
aryarobyy <robyaryanata12345@gmail.com>
2025-11-15 09:09:44 +0700

    (+) validation

--- backend/utils/helper/validation.go
+++ backend/utils/helper/validation.go
@@ -73,3 +73,19 @@ func GetPaginationQuery(c *gin.Context, defaultLimit, defaultOffset int) (int, i
 
 	return limit, offset, nil
 }
+
+func DetectLoginType(id string) string {
+	var (
+		nidnRegex = regexp.MustCompile(`^0\d{9}$`)
+		nimRegex  = regexp.MustCompile(`^(G1A0\d{4}|Y1G0\d{5})$`)
+	)
+
+	switch {
+	case nidnRegex.MatchString(id):
+		return "nidn"
+	case nimRegex.MatchString(id):
+		return "nim"
+	default:
+		return "username"
+	}
+}
